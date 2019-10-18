27b92e1555b5a7c3c737be79ed0a2937e34f9782
Abdullah H. AlShaikh <Abdullah@AlShai5.com>
2019-10-18 14:27:59 +0300

    Added map.

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,24 @@
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap'
 import 'jquery'
-
 import '../css/main.css'
 
 console.log("eOcean: R&R")
+
+window.require([
+    "esri/Map",
+    "esri/views/MapView"
+], function (Map, MapView) {
+
+    var map = new Map({
+        basemap: "dark-gray-vector"
+    });
+
+    var view = new MapView({
+        container: "mapView",
+        map: map,
+        center: [47.990341, 29.378586],
+        zoom: 8
+    });
+
+});
